68d11978c98070f51bf9e471668454a49742085f
Vyas <apurva.vyas@siemens-healthineers.com>
2023-05-27 16:40:04 +0530

    init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ServicesComponent } from './services/services.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { HomeComponent } from './home/home.component';
+import { AdmissionsComponent } from './admissions/admissions.component';
+import { ContactComponent } from './contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'',component:HomeComponent},
+  {path: 'about', component:AboutPageComponent},
+  {path: 'services', component:ServicesComponent },
+  {path: 'admission', component:AdmissionsComponent },
+  {path: 'contact', component:ContactComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BsNavbarComponent } from './bs-navbar/bs-navbar.component';
+import { FeaturesComponent } from './features/features.component';
+import { FooterComponent } from './footer/footer.component';
+import { BrowserModule }  from '@angular/platform-browser';
+import { CarouselComponent } from './carousel/carousel.component';
+import { AboutPageComponent } from './about-page/about-page.component';
+import { ServicesComponent } from './services/services.component';
+import { HomeComponent } from './home/home.component';
+import { AdmissionsComponent } from './admissions/admissions.component';
+import { FormsModule }   from '@angular/forms';
+import { ContactComponent } from './contact/contact.component';
+import { CommonModule } from '@angular/common';
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BsNavbarComponent,    
+    FeaturesComponent,
+    FooterComponent,
+    CarouselComponent,
+    AboutPageComponent,
+    ServicesComponent,
+    HomeComponent,
+    AdmissionsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    CommonModule
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
