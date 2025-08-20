ceff3324697a275e3ec20cc483413a1abac34de2
Apurva Vyas <Apurva.Vyas@ivanti.com>
2025-08-20 14:19:21 +0530

    add pdfs docs

--- src/app/cbse/cbse.component.ts
+++ src/app/cbse/cbse.component.ts
@@ -9,23 +9,23 @@ import { Component } from '@angular/core';
 export class CBSEComponent {
 
   documents = [
-    { name: 'As per Board Requirements', file: 'as-per-board-requirements.pdf' },
-    { name: 'Affiliation Letter', file: 'affiliation-letter.pdf' },
+    { name: 'As per Board Requirements', file: 'AsPerBoardRequirements.pdf' },
+    { name: 'Affiliation Letter', file: 'AffilationLetter.pdf' },
     { name: 'NOC', file: 'noc.pdf' },
-    { name: 'Building Safety', file: 'building-safety.pdf' },
-    { name: 'Self System Generated Certificate', file: 'self-system-certificate.pdf' },
-    { name: 'Fee Structure', file: 'fee-structure.pdf' },
-    { name: 'School Management Committee', file: 'school-management-committee.pdf' },
-    { name: 'Last 3 Year Result of Board Examination', file: 'last-3year-results.pdf' },
-    { name: 'Infrastructure Details', file: 'infrastructure-details.pdf' },
-    { name: 'Certificate of Recognition', file: 'certificate-of-recognition.pdf' },
-    { name: 'Society Registration', file: 'society-registration.pdf' },
-    { name: 'Fire and Safety', file: 'fire-and-safety.pdf' },
-    { name: 'Land Certificate', file: 'land-certificate.pdf' },
-    { name: 'Water and Health Sanitation', file: 'water-health-sanitation.pdf' },
-    { name: 'Annual Academic Calendar', file: 'academic-calendar.pdf' },
-    { name: 'Parents Teachers Association', file: 'pta.pdf' },
-    { name: 'Staff Details', file: 'staff-details.pdf' },
-    { name: 'Self Affidavit of School', file: 'self-affidavit.pdf' }
+    { name: 'Building Safety', file: 'BuildingSafetyCrt.pdf' },
+    { name: 'Self System Generated Certificate', file: 'SelfGenerateCertificate.pdf' },
+    { name: 'Fee Structure', file: 'FeeStructure.pdf' },
+    { name: 'School Management Committee', file: 'SMC.pdf' },
+    { name: 'Last 3 Year Result of Board Examination', file: 'Last3yearResults.pdf' },
+    { name: 'Infrastructure Details', file: 'INFRASTRUCTURE.pdf' },
+    { name: 'Certificate of Recognition', file: 'RecognitionCrt.pdf' },
+    { name: 'Society Registration', file: 'SocietyRegistrationCrt.pdf' },
+    { name: 'Fire and Safety', file: 'FireSafetyCrt.pdf' },
+    { name: 'Land Certificate', file: 'LandCertificate.pdf' },
+    { name: 'Water and Health Sanitation', file: 'WaterSanitationCrt.pdf' },
+    { name: 'Annual Academic Calendar', file: 'AnnualCalendar.pdf' },
+    { name: 'Parents Teachers Association', file: 'PTA.pdf' },
+    { name: 'Staff Details', file: 'StaffDetails.pdf' },
+    { name: 'Self Affidavit of School', file: 'SelfAffidavit.pdf' }
   ];
 }
