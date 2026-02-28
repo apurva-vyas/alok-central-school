29a226ec8d102dcf6a6f37aa894aa75845f7ba49
apurva-vyas <apurvanca@gmail.com>
2026-02-28 19:27:30 +0530

    Remove environment files from .gitignore and add new environment.ts file with YouTube Data API key configuration.

---
+++ src/environments/environment.ts
@@ -0,0 +1,15 @@
+export const environment = {
+  production: false,
+  /*
+   * YouTube Data API v3 Key
+   *
+   * How to get your API key:
+   * 1. Go to https://console.cloud.google.com/
+   * 2. Create a project (or select existing one)
+   * 3. Enable "YouTube Data API v3" from APIs & Services > Library
+   * 4. Go to APIs & Services > Credentials > Create Credentials > API Key
+   * 5. (Recommended) Restrict the key to "YouTube Data API v3" and your domain
+   * 6. Paste the key below
+   */
+  youtubeApiKey: 'AIzaSyBxLwcpkvL0sylWlv0-0mx5GD6zwCKjlkI',
+};
